c3544439cdfbb91766b26e7595ab1b1a2bc501b4
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-11-05 22:23:12 +0000

    components and pages added

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import css from './Loader.module.css';
 const Loader = () => {
   return (
     <div className={css.loader}>
-      <InfinitySpin width="200" color='#407BFF' />
+      <InfinitySpin width="200" color='#e44848' />
     </div>
   );
 };
